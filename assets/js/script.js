960025c81c3097cbb72ac07f6323fe5c5a3065ea
Anna Kaser <kaserann96@gmail.com>
2020-08-21 21:35:17 -0600

    timer and questions loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,87 +1,7 @@
-var startButton = document.getElementById('start-btn')
-var nextButton = document.getElementById('next-btn')
-var questionContainerEl = document.getElementById('question-container')
-var questionEl = document.getElementById('question')
-var answerButtonsEl = document.getElementById('answer-buttons')
+let CORRECT = 0;
+var currentQuestionIndex = 0; //Globally scoped question counter
 
-startButton.addEventListener('click', startQuiz)
-answerButtonsEl.addEventListener('click', () => {
-    setNextQuestion()
-})
-
-function startQuiz() {
-    startButton.classList.add('hide')
-    questionContainerEl.classList.remove('hide')
-    setNextQuestion()
-}
-
-function setNextQuestion() {
-    resetState()
-    showQuestion(questions);
-}
-
-function showQuestion() {
-    questionEl.innerText = questions[0].question;
-
-    questions[0].answers.forEach(answers => {
-        var button = document.createElement('button')
-        button.innerText = answers.text
-        button.classList.add('btn')
-        if (answers.correct) {
-            button.dataset.correct = answers.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsEl.appendChild(button)
-    })
-}
-
-function resetState() {
-    nextButton.classList.add('hide')
-    while (answerButtonsEl.firstChild) {
-        answerButtonsEl.removeChild(answerButtonsEl.firstChild)
-    }
-}
-
-function selectAnswer() {
-
-    let ans = 0;
-
-    for(var i = 0; i < answers.length; i++)
-{
-    var answer = confirm(question[0].answers)
-
-    if (answer === question[0].answer) {
-        alert("nice!");
-        ans++;
-    }
-    else{
-        alert("nope!");
-    }
-}
-    // var selectedButton = e.target
-    // var correct = selectedButton.dataset.correct
-    // // setStatusClass(document.body, correct)
-    // Array.from(answerButtonsEl.children).forEach(button => {
-    //     setStatusClass(button, button.dataset.correct)
-    // })
-}
-
-// function setStatusClass(el, correct) {
-//     clearStatusClass(el)
-//     if(correct) {
-//         el.classList.add('correct')
-//     }
-//     else {
-//         el.classList.add('incorrect')
-//     }
-// }
-
-// function clearStatusClass(el) {
-//     el.classList.remove('correct')
-//     el.classList.remove('incorrect')
-// }
-
-var questions = [
+const QUESTIONS = [
     {
         question: 'Commonly used data types DO NOT include:',
         answers: [
@@ -128,3 +48,99 @@ var questions = [
         ]
     }
 ]
+
+var startButton = document.getElementById('start-btn')
+var nextButton = document.getElementById('next-btn')
+var questionContainerEl = document.getElementById('question-container')
+var questionEl = document.getElementById('question')
+var answerButtonsEl = document.getElementById('answer-buttons')
+var welcomeMsg = document.getElementById("start-page")
+var timer = 75
+var mytimer = document.getElementById("my-timer")
+mytimer.innerText = timer
+
+var countdown = function () {
+
+    var timeInterval = setInterval(function () {
+
+        timer > 0 || currentQuestionIndex < 4 ? timer -= 1 : clearInterval(timeInterval);
+
+    }, 1000);
+};
+
+var countdowndisplay = function () { setInterval(function () { mytimer.innerText = timer; }, 1000); }
+
+startButton.addEventListener('click', startQuiz)
+answerButtonsEl.addEventListener('click', () => {
+    setNextQuestion()
+})
+
+function startQuiz() {
+    startButton.classList.add('hide')
+    questionContainerEl.classList.remove('hide')
+    welcomeMsg.classList.add('hide')
+    setNextQuestion()
+    countdown()
+    countdowndisplay()
+}
+
+function setNextQuestion() {
+    resetState()
+    //showQuestion(QUESTIONS);
+    showQuestion(currentQuestionIndex);
+}
+
+function showQuestion(currentQuestionIndex) {
+    if (currentQuestionIndex === 4) {
+        endScore();
+    }
+    questionEl.innerText = QUESTIONS[currentQuestionIndex].question;
+    QUESTIONS[currentQuestionIndex].answers.forEach(answers => {
+        var button = document.createElement('button')
+        button.innerText = answers.text
+        button.value = answers.text
+        button.classList.add('btn')
+        if (answers.correct) {
+            button.dataset.correct = answers.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsEl.appendChild(button)
+    })
+}
+
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild(answerButtonsEl.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    //console.log(e.target.value);
+
+    var answer = QUESTIONS[currentQuestionIndex].answers
+    var check = answer.filter(obj => {
+        return obj.text === e.target.value;
+    });
+    //console.log(check[0].correct);
+
+    if (check[0].correct) {
+        alert("Right");
+        //correct++;
+    }
+    else {
+        alert("Wrong");
+        timer = timer - 5;
+    }
+    currentQuestionIndex++;
+
+    showQuestion(currentQuestionIndex);
+
+
+}
+
+function endScore() {
+    var initials = window.prompt("Your final score is " + timer + ". Please enter your initials to save.");
+    localStorage.setItem("score", initials + " - " + timer);
+    return false;
+}
